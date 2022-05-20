1276b0563fdbfaf36cede915ed2b4deeff037e49
Kanstantsin Krauchuk <konstantinksh@gmail.com>
2022-05-21 00:39:20 +0300

    feat: start solving 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,10 @@
+const path = require('node:path');
+const fs = require('node:fs');
+
+const pathToNewDir = path.join(__dirname, 'files-copy');
+
+fs.mkdir(pathToNewDir, {recursive: true}, (err) => {
+  if (err) throw err;
+  fs.copyFile
+});
 
