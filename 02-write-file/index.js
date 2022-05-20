0c6ceade65e7b12467a3394b61b503676fb70755
Kanstantsin Krauchuk <konstantinksh@gmail.com>
2022-05-20 19:32:03 +0300

    feat: solve 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,10 +1,29 @@
-const path = require('path');
-const fs = require('fs');
+const path = require('node:path');
+const fs = require('node:fs');
+const readline = require('node:readline');
+const { stdin: input, stdout: output} = require('node:process');
 
-fs.writeFile(
-  path.join(__dirname, 'text.txt'),
-  'Hello world',
-  (err) => {
-    if (err) throw err;
+const filePath = path.join(__dirname, 'text.txt');
+const writeableStream = fs.createWriteStream(filePath, 'utf-8');
+
+const rl = readline.createInterface({ input, output });
+
+output.write('Dear Sir or Madame, write some text down below, if you please:\n');
+
+const closeCommand = function() {  
+  output.write('Thx, seeYa!');
+  rl.close();
+}
+
+rl.on('SIGINT', closeCommand);
+rl.on('line', (text) => {
+  if (text.toString().trim() === 'exit') {
+    closeCommand();
+  } else {
+    writeableStream.write(`${text}\n`);
   }
-);
+});
+rl.on('error', error => console.log('Error', error.message));
+
+
+
