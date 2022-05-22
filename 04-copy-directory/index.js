8e413b93fbb0900e8d4f17e9a11cd478e18113bf
Kanstantsin Krauchuk <konstantinksh@gmail.com>
2022-05-22 13:13:07 +0300

    feat: solve 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,10 +1,45 @@
 const path = require('node:path');
 const fs = require('node:fs');
 
-const pathToNewDir = path.join(__dirname, 'files-copy');
+const pathToSourceDir = path.resolve(__dirname, 'files');
+const pathToDestDir = path.resolve(__dirname, 'files-copy');
 
-fs.mkdir(pathToNewDir, {recursive: true}, (err) => {
-  if (err) throw err;
-  fs.copyFile
-});
+const removeDir = (source, dest) => {
+  fs.rm(dest, {force: true, recursive: true}, (err) => {
+    if (err) throw err;
+    createDir(source, dest);
+  });
+};
 
+const createDir = (source, dest) => {
+  fs.mkdir(dest, {recursive: true}, (err) => {
+    if (err) throw err;
+    readDir(source, dest);
+  });};
+
+const readDir = (source, dest) => {
+  fs.readdir(source, {withFileTypes: true}, (err, files) => {
+    if (err) throw err;
+    files.forEach(el => {
+      let newSource = path.join(source, el.name);
+      let newDest = path.join(dest, el.name);
+      if (el.isFile()) {
+        copyFile(newSource, newDest);
+      } else if (el.isDirectory()) {
+        copyDir(newSource, newDest);
+      }
+    });
+  });
+};
+
+const copyFile = (source, dest) => {
+  fs.copyFile(source, dest, (err) => {
+    if (err) throw err;
+  });
+};
+
+const copyDir = (source, dest) => {
+  removeDir(source, dest);
+};
+
+copyDir(pathToSourceDir, pathToDestDir);
