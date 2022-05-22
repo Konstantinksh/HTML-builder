2813448c45e07b943c1bf371c2e8e0f2fbaaca2b
Kanstantsin Krauchuk <konstantinksh@gmail.com>
2022-05-23 00:07:17 +0300

    feat: solve 06-build-page

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -4,12 +4,12 @@ const fs = require('node:fs');
 const pathToAssetsDir = path.resolve(__dirname, 'assets');
 const pathToComponentsDir = path.resolve(__dirname, 'components');
 const pathToStylesDir = path.resolve(__dirname, 'styles');
-const pathToHTML = path.resolve(__dirname, 'template.html');
+const pathToHTMLTemplate = path.resolve(__dirname, 'template.html');
 const pathToDestDir = path.resolve(__dirname, 'project-dist');
+const pathToDestCSS = path.resolve(pathToDestDir, 'style.css');
+const pathToDestHTML = path.resolve(pathToDestDir, 'index.html');
 
-const pathToDestCss = path.resolve(pathToDestDir, 'style.css');
-const writeableStreamCSS = fs.createWriteStream(pathToDestCss, 'utf-8');
-
+// COPY FOLDER
 const removeDir = (source, dest) => {
   fs.rm(dest, {force: true, recursive: true}, (err) => {
     if (err) throw err;
@@ -20,9 +20,7 @@ const removeDir = (source, dest) => {
 const createDir = (source, dest) => {
   fs.mkdir(dest, {recursive: true}, (err) => {
     if (err) throw err;
-
     readDir(source, dest);
-    // bundleCSS(pathToStylesDir);
   });
 };
 
@@ -52,36 +50,71 @@ const copyDir = (source, dest) => {
 };
 
 
-
-const bundleCSS = (src) => {
-  readCSSFiles(src);
+// CSS Bundle
+const bundleCSS = (src) => {  
+  const writeableStreamCSS = fs.createWriteStream(pathToDestCSS, 'utf-8');
+  readCSSFiles(src, writeableStreamCSS);
 };
 
-const readCSSFiles = (src) => {
+const readCSSFiles = (src, dest) => {
   fs.readdir(src, {withFileTypes: true}, (err, files) => {
     if (err) throw err;
     files.forEach(el => {
       if (el.isFile() && (path.extname(el.name) === '.css')) {
         const currentFile = path.join(src, el.name);
-        readCurrentFile(currentFile);
+        // console.log(el.name);
+        readCurrentFile(currentFile, dest);
       }
     });
   });
 };
 
-const readCurrentFile = (file) => {
+const readCurrentFile = (file, dest) => {
   fs.readFile(file, (err, data) => {
     if (err) throw err;
-    writeToDest(data);
+    writeToDest(data, dest);
   });
 };
 
-const writeToDest = (data) => {  
-  writeableStreamCSS.write(`${data}\n`);
+const writeToDest = (data, dest) => {
+  dest.write(`${data}\n`);
 };
 
+// BUNDLE HTML
+const bundleHTML = () => {
+  let innerHTML = '';
+  const newReadStream = fs.createReadStream(pathToHTMLTemplate);
+
+  newReadStream.on('data', data => {
+    innerHTML = data.toString();
+    fs.readdir(path.join(pathToComponentsDir), {withFileTypes: true}, (err, res) => {
+      if (err) throw err;
+      res.forEach(el => {
+        if (el.isFile() && path.extname(el.name) === '.html') {
+          const pathToCurrentFile = path.join(pathToComponentsDir, el.name);
+          fs.readFile(pathToCurrentFile, (err, data) => {
+            if (err) throw err;
+            innerHTML = innerHTML.replace(`{{${el.name.slice(0, -5)}}}`, data.toString());
+            const newHtml = fs.createWriteStream(pathToDestHTML);
+            newHtml.write(innerHTML);
+          });
+        }
+      });
+    });
+  });
+};
+
+// MAIN FUNCTION
 const buildPage = () => {
-  copyDir(pathToAssetsDir, path.join(pathToDestDir, 'assets')); //copy assets  
+  fs.rm(pathToDestDir, {force: true, recursive: true}, (err) => {
+    if (err) throw err;
+    fs.mkdir(pathToDestDir, {recursive: true}, (err) => {
+      if (err) throw err;
+      copyDir(pathToAssetsDir, path.join(pathToDestDir, 'assets')); //copy assets  
+      bundleCSS(pathToStylesDir);
+      bundleHTML();
+    });
+  });
 };
 
 buildPage();
